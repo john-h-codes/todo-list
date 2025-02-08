82ff89392c5330cc980cb8726d0fe70bbf55bf15
john-h-codes <johnhambleton860@gmail.com>
2025-02-08 12:54:23 -0600

    initial commit: display_menu function

---
+++ main.py
@@ -0,0 +1,9 @@
+# Function to display a menu
+def display_menu():
+    print('To-Do List Application')
+    print('1. View Tasks')
+    print('2. Add Task')
+    print('3. Delete Task')
+    print('4. Exit')
+
+    
